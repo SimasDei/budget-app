d0fbfafaf12859b630db7520db80cea0d29e4d65
SimasDei <simondeiman@gmail.com>
2019-02-10 15:22:14 +0200

    add event listeners, assign them to a DOM object, add initialization method

--- app.js
+++ app.js
@@ -1,27 +1,70 @@
+// <==== BUDGET CONTROLLER ====>
 const budgetController = (function() {
-  let x = 23;
+  // Code4US
+})();
 
-  let add = function(a) {
-    return x + a;
+// <==== UI CONTROLLER ====>
+const UIController = (function() {
+  let DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputButton: '.add__btn'
   };
 
   return {
-    publicTest: function(b) {
-      return add(b);
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
     }
   };
 })();
 
-const UIController = (function() {
-  // Code goes here
-})();
-
+// <==== GLOBAL CONTROLLER ====>
+/**
+ * @param {Function} budgetCtrl - budget Controller
+ * @param {Function} UICtrl - UI Controller
+ * @param {Function} ctrlAddItem - Add item method
+ */
 const controller = (function(budgetCtrl, UICtrl) {
-  let y = budgetCtrl.publicTest(5);
+  const setupEventListeners = function() {
+    let DOM = UICtrl.getDOMStrings();
+
+    document
+      .querySelector(DOM.inputButton)
+      .addEventListener('click', ctrlAddItem);
+    /**
+     *  Add global Event Listener for the Return Key
+     * @param {Object} event - keyboard key press
+     */
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  const ctrlAddItem = function() {
+    /**
+     * @TODO - Add item to budgetController
+     * @TODO - Add item to UI
+     * @TODO - Calculate AND Display Budget
+     */
+    let input = UICtrl.getInput();
+  };
 
   return {
-    publicTestTwo: function() {
-      return y;
+    init: function() {
+      console.log('App has Started /o/ dab');
+      setupEventListeners();
     }
   };
 })(budgetController, UIController);
+
+controller.init();
