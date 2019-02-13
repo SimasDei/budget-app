e28331a2bb780c072908f1f9b4a6bee0004d9ace
SimasDei <simondeiman@gmail.com>
2019-02-13 16:45:47 +0200

    add item to list method, generate dynamic html on every event

--- app.js
+++ app.js
@@ -73,7 +73,9 @@ const UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputButton: '.add__btn'
+    inputButton: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return {
@@ -83,6 +85,32 @@ const UIController = (function() {
         description: document.querySelector(DOMStrings.inputDescription).value,
         value: document.querySelector(DOMStrings.inputValue).value
       };
+    },
+    /**
+     *
+     * @param {object} obj - input data
+     * @param {string} type - income or expense
+     * Create HTML string with placeholder text
+     * Replace the placeholder text with data
+     * Insert HTML into the DOM
+     */
+    addListItem: function(obj, type) {
+      let html, newHtml, element;
+      if (type === 'inc') {
+        element = DOMStrings.incomeContainer;
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        element = DOMStrings.expensesContainer;
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
     getDOMStrings: function() {
       return DOMStrings;
@@ -127,11 +155,13 @@ const controller = (function(budgetCtrl, UICtrl) {
       input.description,
       input.value
     );
+
+    UICtrl.addListItem(newItem, input.type);
   };
 
   return {
     init: function() {
-      console.log('App has Started /o/ dab');
+      console.log('App has Started o/');
       setupEventListeners();
     }
   };
